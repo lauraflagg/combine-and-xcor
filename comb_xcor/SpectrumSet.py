516af70ac18800e494331fca1d2f1ca601d7e92a
lauraflagg <laura.flagg1@gmail.com>
2022-01-17 12:41:51 -0500

    broke comb_xcor up into its classes

---
+++ comb_xcor/SpectrumSet.py
@@ -0,0 +1,301 @@
+
+import sys
+from stats_lf import xcor, xcor_fast, chisq, log_likelihood_zucker, gaussian2D
+import numpy as np
+from astro_lf import findbests2n, vel2wl, c_kms,wl2vel,veltodeltawl, getorbitpars
+from readwrite_lf import read2cols
+from PyAstronomy import pyasl
+from scipy import stats
+import matplotlib.pyplot as plt
+import v_curve
+import spectres
+import pandas as pd
+import pickle
+import time
+import astro_lf
+import radvel_rv
+from astropy.io import fits
+
+from matplotlib import patches
+import matplotlib.colors as mpl
+from lmfit import Model
+#from lmfit.lineshapes import gaussian2d
+#from lmfit.models import Gaussian2dModel
+
+import logging
+logfile='debug_log.txt'
+logging.basicConfig(level=logging.INFO,filename=logfile)
+#logging.basicConfig(level=logging.DEBUG,filename=logfile)
+
+
+'''encorporates _upgrade'''
+
+Ms=1.989*10.0**33
+Mj=1.8986*10.0**30
+
+class SpectrumSet:
+    def _read_flux_(self):
+        if self.filename[-3:]=='pic':
+            ob = open(self.loc, "rb")
+            d,dat0=pickle.load(ob)
+            dat=dat0.transpose()
+            self.dates=np.array(d[1:]).astype(np.float)
+            self.data=dat[1:,:].astype(np.float)
+            wls=dat[0]
+        else:
+            flux=pd.read_csv(self.loc,header=None)
+            fluxes=flux.values
+            #fluxes=np.loadtxt(filename,delimiter=',',dtype=str)
+            self.dates=np.array(fluxes[0][1:])
+            #fluxes=np.loadtxt(filename,delimiter=',',skiprows=1)
+            data1=np.transpose(np.array(fluxes[1:]))
+            self.data=data1[1:,:].astype(np.float)
+            
+            wls=data1[:][0]
+        self.wls=wls.astype(np.float)     
+
+    def _read_template_(self):
+        if self.template_fn[-4:]=='fits':
+            '''mantis templatess'''
+            with fits.open(self.template_fn) as f:
+                f0=f[0].data
+            f1=f0[1]
+            fl=f1[::-1]
+            wn=np.arange(len(f0[1]))*0.01+4000
+            wl=10000./wn[::-1]*1e4
+        else:
+            df=pd.read_csv(self.template_fn)
+            data_df=np.transpose(df.values)
+            wl=data_df[0]
+            fl=data_df[1]
+            
+        if self.template_wl_unit!=None:
+            self.template_wl_unit=astro_lf.find_unit(template_wl_unit)
+        else:
+            self.template_wl_unit=astro_lf.guess_unit(wl[0])
+        
+        conversion_fac=astro_lf.wl_unit_choices[self.template_wl_unit].conversion/astro_lf.wl_unit_choices[self.spectrum_wl_unit].conversion
+        wl_co_temp_0=wl*conversion_fac
+
+        return wl_co_temp_0,fl
+    
+
+    def __init__(self,filename,folder,bad_dates=None,maskfile=None,template_fn='template_width0p2_CO.csv',subset='',badphases=[],
+                 period=8.9891, scale=1.,day0=2453367.805,template_wl_unit=None,spectrum_wl_unit=None,wllims=[0.0,1e20],subtractone=True,transit_midpoint=None,tp=None,printphases=False):
+        #only need to worry abot day0, period if badphases!=[]
+        #subbtractone added to deal with transmission spectra
+        self.idnum=filename[-12:-4]
+        self.filename=filename
+        self.folder=folder
+        self.bad_date=bad_dates
+        self.maskfile=maskfile
+        self.template_fn=template_fn
+        self.subset=subset
+        self.badphases=badphases
+        self.period=period
+        self.scale=scale
+        self.day0=day0
+        self.ccarrs=[]
+        self.loc=folder+filename
+        self.template_wl_unit=template_wl_unit
+        self.wllims=wllims
+        self._read_flux_()
+        if tp==None:
+            self.tp=day0
+        else:
+            self.tp=tp
+        if transit_midpoint==None:
+            self.transit_midpoint=day0
+        else:
+            self.transit_midpoint=transit_midpoint
+        
+        
+
+            
+        if spectrum_wl_unit!=None:
+            self.spectrum_wl_unit=astro_lf.find_unit(spectrum_wl_unit)
+        else:
+            self.spectrum_wl_unit=astro_lf.guess_unit(self.wls[0])
+        
+
+        if bad_dates==None:
+            hjdall=self.dates
+            bad_dates=[]
+        else:			
+            temp = pd.read_csv(folder+'hhjd'+subset+'.csv')
+            temp2=np.transpose(temp.values)
+            hjdall=temp2[1]
+        
+        
+
+        if badphases!=[]:
+            i=0
+            
+            while i<len(self.dates):
+                ba=False
+                logging.debug('day: ',hjdall[i])
+   
+
+                pha=((hjdall[i]-day0)/period) % 1.0 
+
+                    
+ 
+                if badphases[4](badphases[0](pha,badphases[1]), badphases[2](pha,badphases[3])):    
+                    bad_dates.append(str(int(self.dates[i])))
+                    bad_dates.append(str(float(self.dates[i])))
+                    ba=True
+                if len(badphases)>5:
+                    if badphases[9](badphases[5](pha,badphases[6]), badphases[7](pha,badphases[8])):
+                        bad_dates.append(str(int(self.dates[i])))
+                        bad_dates.append(str(float(self.dates[i])))
+                        ba=True
+                if printphases:
+                    print(hjdall[i],pha,'bad? ',ba)
+
+                i=i+1  
+
+
+
+        idnum=filename[-12:-4]
+
+        goodlocs=[]
+        l=len(self.dates)
+        i=0
+
+        bad_dates=np.array(bad_dates).astype(np.float)
+        #print(bad_dates)
+
+
+        while i<l:
+            if (float(self.dates[i]) not in bad_dates)==True:
+                goodlocs.append(i)
+            i=i+1    
+
+
+        #print(goodlocs)
+
+        medstar=np.median(self.data[goodlocs],0)
+        meanstar=np.mean(self.data[goodlocs],0)
+        l_good=len(goodlocs)
+
+
+
+        badwls=np.array([])
+        starts,ends=read2cols(maskfile)
+        for s,e in zip(starts,ends):
+            arr=np.where((self.wls>s) & (self.wls<=e))
+            badwls=np.append(badwls,arr)
+        arr=np.where(self.wls<self.wllims[0])
+        badwls=np.append(badwls,arr)
+        arr=np.where(self.wls>self.wllims[1])
+        badwls=np.append(badwls,arr)        
+
+        badwls=badwls.astype(np.int)    
+
+
+        gooddat=self.data[goodlocs]
+
+
+        hjd=hjdall[goodlocs]   
+
+        #read in template file
+
+        wl_co_temp_0, fl_co_temp=self._read_template_()
+        
+        ccarr=[]
+        cc2arr=[]
+        sigarr=[]
+        sig2arr=[] 
+        
+        logging.debug('wl_template:',wl_co_temp_0[0:10],wl_co_temp_0[-10:])
+        logging.debug('wls:',self.wls[0:10],self.wls[-10:])
+        if subtractone:
+            fl_co=spectres.spectres(self.wls, wl_co_temp_0, fl_co_temp,verbose=False,fill=1.)-1.0
+        else:
+            fl_co=spectres.spectres(self.wls, wl_co_temp_0, fl_co_temp,verbose=False)
+        
+        
+        fl_co[badwls]=0
+        fl_co=fl_co*scale
+
+        plot=1
+        self.hjd=hjd
+        self.badwls=badwls
+        self.fl_co=np.ma.masked_equal(fl_co,0)
+        self.gooddata=np.ma.masked_equal(gooddat,0)
+        self.phases=((self.hjd-day0)/period) % 1.0 
+        self.phases[self.phases>.5]=self.phases[self.phases>.5]-1.
+        #good data
+    
+    def filtertemplate(self,flip=False,butter=False,resample=1):
+        '''resample is the ratio between the old resolution and the new resolution'''
+        if flip:
+            self.fl_co=-self.fl_co
+        if butter:
+            None
+            
+    def plottemplate(self,wlrange=None):
+
+        if wlrange!=None:
+            loc=((self.wls>=wlrange[0]) & (self.wls<=wlrange[1]))
+            wlplot=self.wls[loc]
+            datplot=self.fl_co[:,loc]
+        else:
+            wlplot=self.wls
+            datplot=self.fl_co
+        fig=plt.figure(figsize=(8,5))
+        axarr = fig.add_subplot(1,1,1)        
+        x=axarr.plot(wlplot,datplot)
+      
+        
+        axarr.set_ylabel('flux')
+        axarr.set_xlabel('wavelength (microns)')
+    
+
+    def plotspecs(self,wlrange=None,orbitalpars=None,centwl=None,code='vcurve',vsys=0,lcolor='white',nontransitingphases=False):
+        
+        if nontransitingphases:
+            self.phases[self.phases<0]=self.phases[self.phases<0]+1
+        
+        if wlrange!=None:
+            loc=((self.wls>=wlrange[0]) & (self.wls<=wlrange[1]))
+            wlplot=self.wls[loc]
+            datplot=self.gooddata[:,loc]
+        else:
+            wlplot=self.wls
+            datplot=self.gooddata
+        fig=plt.figure(figsize=(8,5))
+        axarr = fig.add_subplot(1,1,1)        
+        x=axarr.contourf(wlplot,self.phases,datplot)
+        
+        if centwl!=None:
+            rvshifts=np.array([planetrvshift(date,orbitalpars,day0=self.day0,code=code)+vsys for date in self.hjd])
+            wlline=astro_lf.veltodeltawl(rvshifts,centwl)+centwl
+
+            axarr.plot(wlline,self.phases,color=lcolor,lw=2,zorder=40)
+        
+        
+        axarr.set_ylabel('phase')
+        axarr.set_xlabel('wavelength (microns)')
+        temp=fig.colorbar(x)
+        
+        temp.ax.set_ylabel(r'relative flux')
+        
+    def maskwavelengths(self,wlcent,wlwid):
+        '''wlcent, wlwid in microns'''
+        loc=((self.wls<=(wlcent+wlwid)) & (self.wls>=(wlcent-wlwid)))
+        self.fl_co=np.ma.masked_where(loc,self.fl_co)
+        self.gooddata[:,loc]=0.
+        self.gooddata=np.ma.masked_equal(self.gooddata,0)
+        
+
+    
+    def system_properties(self, k_s,M_star,M_planet=.0001):
+        '''M_star is stellar mass in solar masses
+        k_s is stellar velocity amplitude in km/s
+        M_planet is planet mass in Jupiter masses'''
+        self.k_s=k_s
+        self.M_star=M_star
+        self.M_cgs=M_star*Ms
+        self.M_planet=M_planet
+        self.M_planet_cgs=M_planet*Mj
---
+++ comb_xcor/__init__.py
@@ -0,0 +1,37 @@
+
+import sys
+from stats_lf import xcor, xcor_fast, chisq, log_likelihood_zucker, gaussian2D
+import numpy as np
+from astro_lf import findbests2n, vel2wl, c_kms,wl2vel,veltodeltawl, getorbitpars
+from readwrite_lf import read2cols
+from PyAstronomy import pyasl
+from scipy import stats
+import matplotlib.pyplot as plt
+import v_curve
+import spectres
+import pandas as pd
+import pickle
+import time
+import astro_lf
+import radvel_rv
+from astropy.io import fits
+
+from matplotlib import patches
+import matplotlib.colors as mpl
+from lmfit import Model
+#from lmfit.lineshapes import gaussian2d
+#from lmfit.models import Gaussian2dModel
+
+import logging
+logfile='debug_log.txt'
+logging.basicConfig(level=logging.INFO,filename=logfile)
+#logging.basicConfig(level=logging.DEBUG,filename=logfile)
+
+
+'''encorporates _upgrade'''
+
+Ms=1.989*10.0**33
+Mj=1.8986*10.0**30
+
+from comb_xcor.SpectrumSet import SpectrumSet 
+from comb_xcor.ccfmatrix import CCFMatrix
